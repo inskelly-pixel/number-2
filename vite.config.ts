534519959f861e9505e7cc713bd65a94ceef26d9
김은결 <kyeolrii@gim-eungyeol-ui-MacBookPro.local>
2025-11-06 20:25:19 +0900

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,21 +4,27 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 import fs from "fs";
 
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-     https: {
-      key: fs.readFileSync("localhost-key.pem"),
-      cert: fs.readFileSync("localhost.pem"),
+  return {
+    server: isDev
+      ? {
+          host: "::",
+          https: {
+            key: fs.readFileSync("localhost-key.pem"),
+            cert: fs.readFileSync("localhost.pem"),
+          },
+          port: 5174,
+        }
+      : undefined,
+
+    plugins: [react(), isDev && componentTagger()].filter(Boolean),
+
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-    port: 5174,
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+  };
+});
