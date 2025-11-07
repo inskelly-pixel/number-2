b7ec1beeebf722f79b194ceff2aa63f971d0f618
김은결 <kyeolrii@gim-eungyeol-ui-MacBookPro.local>
2025-11-07 21:18:35 +0900

    Remove LFS for mapText.ts

--- src/data/mapText.ts
+++ src/data/mapText.ts
@@ -1,3 +1,16 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5c99b98662d3c5a920db0a104b1ad13823898625b2bd48fde5b2bf8b1c77f623
-size 272
+export const mapText = [
+  {
+    label: "KOREA",
+    ko: ["서울", "부산"],
+    en: ["Seoul", "Busan"],
+    x: "40%",   // optional
+    y: "30%"
+  },
+  {
+    label: "EUROPE",
+    ko: ["파리", "런던"],
+    en: ["Paris", "London"],
+    x: "20%",
+    y: "45%",
+  }
+];
