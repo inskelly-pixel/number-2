2185e6cdb7494691b5a0e1cff09f63da3cd4a7dc
김은결 <kyeolrii@gim-eungyeol-ui-MacBookPro.local>
2025-11-07 20:36:34 +0900

    Fix: update ProjectDetailMediaSection.tsx

--- src/pages/ProjectDetailMediaSection.tsx
+++ src/pages/ProjectDetailMediaSection.tsx
@@ -32,7 +32,7 @@ const ProjectDetailMediaSection: React.FC<Props> = ({
     return (
       <ProjectDetailMedia
         video="https://player.vimeo.com/video/1124760235?h=8e72d8e123&badge=0&autopause=0&player_id=0&app_id=58479"
-        svgs={["/images/Frame 30.svg", "/images/Frame34.svg"]}
+        svgs={["/images/seoul.svg"]}
         title={title}
         description={"hi"}
         description5={description5 || ""}
