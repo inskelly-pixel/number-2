ca6d94a23b9f88e6b9db1dde0194e319e4adea5a
김은결 <kyeolrii@gim-eungyeol-ui-MacBookPro.local>
2025-11-07 20:57:54 +0900

    Fix: update ProjectDetailMediaSection.tsx

--- src/pages/ProjectDetailMediaSection.tsx
+++ src/pages/ProjectDetailMediaSection.tsx
@@ -21,7 +21,7 @@ const ProjectDetailMediaSection: React.FC<Props> = ({
         video="https://player.vimeo.com/video/1125201347?badge=0&autopause=0&player_id=0&app_id=58479"
         svgs={["/images/image144.svg", "/images/Group569.svg"]}
         title={title}
-        description={"hello"}
+        description={"Each chapter unfolds and takes shape within a digital flow. The letters are visualized through distinct abstract movements and rhythms, turning the narrative into motion. As the story progresses—and with each click—the movements gradually expand and intensify."}
         description5={description5 || ""}
 
       />
@@ -34,7 +34,7 @@ const ProjectDetailMediaSection: React.FC<Props> = ({
         video="https://player.vimeo.com/video/1124760235?h=8e72d8e123&badge=0&autopause=0&player_id=0&app_id=58479"
         svgs={["/images/seoul.svg"]}
         title={title}
-        description={"hi"}
+        description={"This web page visualizes Seoul’s densely saturated districts through distorted interpretations. Each area was explored firsthand to analyze its distinctive characteristics, from which three representative keywords were derived. Using a collage approach, the project aims to convey the complex and multifaceted nature of each district."}
         description5={description5 || ""}
 
       />
@@ -47,8 +47,8 @@ const ProjectDetailMediaSection: React.FC<Props> = ({
         video="https://player.vimeo.com/video/1124735859?badge=0&autopause=0&player_id=0&app_id=58479"
         svgs={["/images/image144.svg", "/images/Group569.svg"]}
         title={title}
-        description={"you"}
-        description5={description5 || "This"}
+        description={"In a resonant space where multiple voices overlap, I recorded spoken sounds and created a video based on them. By visualizing how sound gradually fragments and dissolves within a large space, the work conveys the spatial qualities of its acoustic environment."}
+        description5={description5 || ""}
 
       />
     );
